3a771fca97b2e687675de6ecc69d483ad251e902
Daniel Weinmann <danielweinmann@gmail.com>
2023-06-02 16:12:40 -0300

    Expose getJson and getText as typedResponse options

--- src/internals.ts
+++ src/internals.ts
@@ -1,27 +1,27 @@
-import { Schema } from './types'
+import type { GetJson, GetText, Schema } from './types'
 
 /**
  * It returns the JSON object or throws an error if the response is not ok.
  * @param response the Response to be parsed
  * @returns the response.json method that accepts a type or Zod schema for a typed json response
  */
-function getJson(response: Response) {
-  return async <T = unknown>(schema?: Schema<T>): Promise<T> => {
+const getJson: GetJson =
+  (response) =>
+  async <T = unknown>(schema?: Schema<T>) => {
     const json = await response.json()
     return schema ? schema.parse(json) : (json as T)
   }
-}
 
 /**
  * @param response the Response to be parsed
  * @returns the response.text method that accepts a type or Zod schema for a typed response
  */
-function getText(response: Response) {
-  return async <T extends string = string>(schema?: Schema<T>): Promise<T> => {
+const getText: GetText =
+  (response) =>
+  async <T extends string = string>(schema?: Schema<T>) => {
     const text = await response.text()
     return schema ? schema.parse(text) : (text as T)
   }
-}
 
 /**
  * This is an enhanced version of the typeof operator to check the type of more complex values.
