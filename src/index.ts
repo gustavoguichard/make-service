4ee12e5b7ffb76a425ed0f0a7f90834850fcd502
Guga Guichard <gustavoguichard@gmail.com>
2025-06-17 17:30:44 -0300

    Update some dev packages

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ export {
   ensureStringBody,
   makeGetApiURL,
   mergeHeaders,
+  ParseResponseError,
   replaceURLParams,
   typeOf,
-  ParseResponseError,
 } from './primitives'
 export type * from './types'
